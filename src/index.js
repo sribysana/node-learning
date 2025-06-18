a850dd3f855af5651d294cac541dd8250e61682a
Srikanth Bysana <sribysana@gmail.com>
2025-06-19 01:20:56 +0530

    parsing http method and parsing query string

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const http = require('http');// import  http module
+const { json } = require('stream/consumers');
 const url = require('url'); // import url module
 
 // use http module to create a server
@@ -9,10 +10,14 @@ const server = http.createServer((req,res)=>{
     const parsedUrl = url.parse(req.url, true); // parse the URL
     const {pathname} = parsedUrl;
     const trimmedPathName = pathname.replace(/^\/+|\/+$/g, ''); // trim leading and trailing slashes
-    console.log('trimmedPathName:', trimmedPathName);
- // console.log('pathname:', pathname);
+    // Extract the HTTP method from the request
+    // This will be used to determine the type of request made
+    const method = req.method;
+    const query =parsedUrl.query; // Extract query parameters from the URL
+    
+    console.log('trimmedPathName:', trimmedPathName,'method:', method,'query:', query);  
     res.writeHead(200, { 'Content-Type': "application/json" });
-    res.end(JSON.stringify({ message: 'Hello, World!' }));
+    res.end(JSON.stringify({ message: { trimmedPathName, method, query } })); // Respond with a JSON object
 });
 
 // The server listens on port 3000
