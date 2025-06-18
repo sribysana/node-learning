e46a8b49f2aa98cd98beb6dc3d72ef5817375f61
Srikanth Bysana <sribysana@gmail.com>
2025-06-19 00:41:15 +0530

    Get request path and trim the first slash, then assign it to a variable

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const http = require('http');// import  http module
+const url = require('url'); // import url module
 
 // use http module to create a server
 // The server will respond with a JSON object when accessed
 
 const server = http.createServer((req,res)=>{
     console.log(`Received request for ${req.url}`);
+    const parsedUrl = url.parse(req.url, true); // parse the URL
+    const {pathname} = parsedUrl;
+    const trimmedPathName = pathname.replace(/^\/+|\/+$/g, ''); // trim leading and trailing slashes
+    console.log('trimmedPathName:', trimmedPathName);
+ // console.log('pathname:', pathname);
     res.writeHead(200, { 'Content-Type': "application/json" });
     res.end(JSON.stringify({ message: 'Hello, World!' }));
 });
