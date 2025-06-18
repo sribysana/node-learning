9b1ef0435743a97517812450add005828d6558b6
Srikanth Bysana <sribysana@gmail.com>
2025-06-19 00:20:52 +0530

    creating basic server with hellow world response

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const http = require('http');// import  http module
+
+// use http module to create a server
+// The server will respond with a JSON object when accessed
+
+const server = http.createServer((req,res)=>{
+    console.log(`Received request for ${req.url}`);
+    res.writeHead(200, { 'Content-Type': "application/json" });
+    res.end(JSON.stringify({ message: 'Hello, World!' }));
+});
+
+// The server listens on port 3000
+// When the server is running, it will log a message to the console
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
